38d6d89f81e18db5ca3438b4acd2f3b657364b7e
Ekamjot Kaur <kaurekam14@gmail.com>
2020-09-04 05:00:33 +0530

    maths

---
+++ scripts/maths.js
@@ -0,0 +1,19 @@
+function functionalities()
+{
+document.write(Math.pow(8, 2));
+document.innerHTML = ' ';
+document.write(Math.sqrt(64));
+document.innerHTML = '<br';
+document.write(Math.abs(-4.7)); 
+document.innerHTML = '<br';
+var x = Math.random();
+document.write(x);
+document.innerHTML = '<br';
+Math.ceil(x);
+document.innerHTML = '<br';
+Math.round(x);
+document.innerHTML = '<br';
+Math.floor(x);
+document.innerHTML = '<br';
+Math.sin(45 * Math.PI / 180); 
+}
