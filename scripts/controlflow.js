5086df8648cf50a3dad8afa629980fe51a1257f1
Ekamjot Kaur <kaurekam14@gmail.com>
2020-09-05 01:23:30 +0530

    errors

--- scripts/controlflow.js
+++ scripts/controlflow.js
@@ -1,19 +1,24 @@
 function fibo()
 {
 var x = document.getElementById("no").value;
-if (x >= 10 && x <= 100){
-	console.log("Yes");
-}
-else{
-	console.log("No");
-}
-var f = 0;
-var s = 1;
-var t = f+s;
-for(var i = 0; i < x; i++){
-	document.write(f+'\n');
-	f = s;
-	s = t;
-	t = f+s;
+try{
+	if (x <= 10 || x >= 100){
+		throw x;
+	}
+
+	var f = 0;
+	var s = 1;
+	var t = f+s;
+	for(var i = 0; i < x; i++){
+		document.write(f + '\n');
+		f = s;
+		s = t;
+		t = f+s;
+	}
+} catch(e) {
+	document.write(e + ' is not in the required range of 10-100');
+} finally {
+    console.log('Program Complete');
+    return false;
 }
 }
